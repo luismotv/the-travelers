32879254bcdd76715a38ee52d2103526233527f7
dinhvinhloc <39650247+dinhvinhloc@users.noreply.github.com>
2020-04-10 09:56:31 -0400

    time

--- js/time.js
+++ js/time.js
@@ -1,8 +1,35 @@
 
 function startTime() {
   var today = new Date();
-  //console.log(today.getTimezoneOffset());
-  tz = today.getTime() + (today.getTimezoneOffset() * 60000) + ((-5) * 3600000);
+  console.log(today.getTimezoneOffset());
+  // Set timezome
+
+  title = document.getElementById('main-title').innerHTML;
+  console.log(title);
+  switch (title) {
+    case ("Travelers - Ecuador"):
+      tz = today.getTime() + (today.getTimezoneOffset() * 60000) + ((-5) * 3600000);
+      coutry = "Ecucador";
+      break;
+    case ("Travelers - Philippines"):
+      tz = today.getTime() + (today.getTimezoneOffset() * 60000) + ((8) * 3600000);
+      coutry = "Philippines";
+      break;
+    case ("Travelers - South Korea"):
+      tz = today.getTime() + (today.getTimezoneOffset() * 60000) + ((9) * 3600000);
+      coutry = "South Korea";
+      break;
+    case ("Travelers - Turkey"):
+      tz = today.getTime() + (today.getTimezoneOffset() * 60000) + ((3) * 3600000);
+      coutry = "Turkey";
+      break;
+    case ("Travelers - Vietnam"):
+      tz = today.getTime() + (today.getTimezoneOffset() * 60000) + ((7) * 3600000);
+      coutry = "Vietnam";
+      break;
+  }
+  
+
   today.setTime(tz);
   var h = today.getHours();
   var m = today.getMinutes();
